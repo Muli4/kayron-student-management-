f8d027983f5d3da3cf9ccc398795035a767a96e0
Muli4 <mulimaurice730@gmail.com>
2025-02-13 22:51:41 +0300

    sql

--- sql/db.sql
+++ sql/db.sql
@@ -139,26 +139,17 @@ CREATE TABLE uniform_prices (
 CREATE TABLE uniform_purchases (
     id INT AUTO_INCREMENT PRIMARY KEY,
     receipt_number VARCHAR(50) NOT NULL,
-    name VARCHAR(255) NOT NULL,
     admission_no VARCHAR(50) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    uniform_type ENUM('Uniform', 'P.E T-Shirt', 'Track Suit 20-26', 'Track Suit 2-32') NOT NULL,
+    quantity INT NOT NULL,
     total_price DECIMAL(10, 2) NOT NULL,
     amount_paid DECIMAL(10, 2) NOT NULL,
     balance DECIMAL(10, 2) NOT NULL,
-    payment_type ENUM('Cash', 'm-pesa','bank-transfer') NOT NULL,
-    purchase_id INT NOT NULL,
+    payment_type ENUM('Cash', 'm-pesa', 'bank-transfer') NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE uniform_purchase_items (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    purchase_id INT,
-    uniform_id INT,
-    quantity INT NOT NULL,
-    subtotal DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (purchase_id) REFERENCES uniform_purchases(id) ON DELETE CASCADE,
-    FOREIGN KEY (uniform_id) REFERENCES uniform_prices(id),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
 
 INSERT INTO uniform_prices (uniform_type, size, price) VALUES
 ('Uniform', 'All Sizes', 1000.00),
