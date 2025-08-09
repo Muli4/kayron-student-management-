51d4cc081f61861921cba09776a2119a05179a11
Muli4 <mulimaurice730@gmail.com>
2025-08-09 14:04:39 +0300

    new

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -8,12 +8,13 @@
         <li><a href="./make-payments.php"><i class='bx bx-credit-card-front'></i> Make Payments</a></li>
         <li><a href="./week.php"><i class='bx bx-book-bookmark'></i>Register Week</a></li>
         <li><a href="./attendance_register.php"><i class='bx bx-eraser'></i> Mark Attendance</a></li>
-        <li><a href="./pay-lunch.php"><i class='bx bx-closet'></i>Pay Lunch</a></li>
+        <li><a href="./pay-lunch.php"><i class='bx bx-credit-card-front'></i>Pay Lunch</a></li>
         <li><a href="./lunch_tracker.php"><i class='bx bx-task'></i> Lunch Tracker</a></li>
-        <li><a href="./view-receipts.php"><i class='bx bx-calendar-check'></i> Receipts</a></li>
-        <!--<li><a href="#"><i class='bx bx-printer'></i> Print Receipt</a></li>
-        <li><a href="#"><i class='bx bx-line-chart'></i> School Exams</a></li>
+        <li><a href="./view-receipts.php"><i class='bx bx-printer'></i>Receipts</a></li>
+        <li><a href="./purchase.php"><i class='bx bx-credit-card-front'></i>Purchases</a></li>
+        <!--<li><a href="#"><i class='bx bx-line-chart'></i> School Exams</a></li>
         <li><a href="#"><i class='bx bx-user-circle'></i> Subordinate Staff</a></li>-->
         <li><a href="logout.php"><i class='bx bx-power-off'></i> Logout</a></li>
     </ul>
 </nav>
+
